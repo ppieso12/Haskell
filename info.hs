8b3bd8f287aa43dcb73a072a31df2e5bffb0ce30
ppieso12 <ppieso07@gmail.com>
2017-05-20 21:05:05 +0200

    Update info.hs

--- info.hs
+++ info.hs
@@ -8,5 +8,31 @@ map f [x1, x2, ..., xn] == [f x1, f x2, ..., f xn]
 map f [x1, x2, ...] == [f x1, f x2, ...]
 
 
+elem_nr (h:_) 0 = h
+elem_nr (_:t) i = elem_nr t (i-1)
+
+dlugosc [] = 0
+dlugosc (_:t) = 1 + dlugosc t
+
+konkatenacja [] l2 = l2
+konkatenacja (h:t) l2 = h : (konkatenacja t l2)
+
+(+++) :: [a] -> [a] -> [a]
+[] +++ lb = lb
+(x:t) +++ lb = x:(+++) t lb
+infixr 5 +++
+
+
 LAMBDA EXPRESSION - anonimowa klasa wew w javie
 map (\x -> x+1) [1..5]
+
+Types that can act like a box can be functors. You can think of a list as a box that has an infinite amount of little compartments 
+and they can all be empty, one can be full and the others empty or a number of them can be full. 
+So, what else has the properties of being like a box? For one, the Maybe a type. In a way, it's like a box that can either 
+hold nothing, in which case it has the value of Nothing, or it can hold one item, like "HAHA", in which case it has a value of Just "HAHA"
+
+class Functor f where  
+    fmap :: (a -> b) -> f a -> f b  
+    
+instance Functor [] where  
+    fmap = map  
